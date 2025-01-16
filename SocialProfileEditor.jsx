acf9f888843b38a59f75d4a6669208f470dfad18
IT Samurai Teacher <127447902+IT-Samurai-Teacher@users.noreply.github.com>
2025-01-15 20:01:41 -0800

    Create SocialProfileEditor.jsx

---
+++ SocialProfileEditor.jsx
@@ -0,0 +1,9 @@
+import SocialProfileEditor from './SocialProfileEditor';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <SocialProfileEditor />
+    </div>
+  );
+}
